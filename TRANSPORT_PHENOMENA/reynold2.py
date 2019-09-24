19e4876575c91d038870c6881f6eac4d9c0394c2
Denis Costa <42891990+DenisCosta@users.noreply.github.com>
2019-09-24 10:37:57 -0300

    Add files via upload

---
+++ TRANSPORT_PHENOMENA/reynold2.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 10 10:25:32 2019
+
+@author: 
+        Prof. Dr. Denis C. L. Costa
+                
+Grupo de Pesquisa: 
+                 Gradiente de Modelagem Matemática e
+                 Simulação Computacional - GM²SC
+                 
+Assunto: 
+        Classidica dos escoamentos
+
+Nome do sript: reynold2.py
+
+Disponível em:
+
+"""
+# Análise do Escoamento
+# Variáveis envolvidas
+
+# Grandezas envolvidas:
+d = 0.04 # Diâmetro do duto (m)
+v = 0.1 # Velocidade do fluido (m/s)
+rho = 1000 # Peso específico do fluido (kg/m³)
+# Viscosidade do Fluido
+mi = 1.0030*10**(-3)
+# Número de Reynold
+Re = (v*rho*d)/mi
+print('==========================')
+print('Re = ',round(Re,3))
+print('==========================')
+print('Classificação:')
+# Loop condicional
+if (Re < 2000):
+    print('Escoamento Laminar')
+elif (2000 <= Re < 2400): 
+    print('Escoamento em Transição')
+elif (Re >= 2400): 
+    print('Escoamento Turbulento')
+print('==========================')  
+print('')
+print('Fim do Programa')
+
+
+
+
